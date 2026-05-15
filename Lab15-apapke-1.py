7896bc92da4d0a1048bcafdaed8a371bd4c57514
Mya Applesauce <myaapplesauce42@gmail.com>
2026-05-14 22:57:33 -0400

    might've fixed a problem. tried to add style.

--- Lab15-apapke-1.py
+++ Lab15-apapke-1.py
@@ -7,12 +7,14 @@ Plotting a shape
 
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
+import numpy as np
 import math
 
 x = np.linspace(-10, 10, 420)
 y = (4 * (x ** 2))
 
 fig, ax = plt.subplots()
+plt.style.use("Solarize_Light2")
 polygon = Polygon([(10, 10), (60, 20), (50, 60)], edgecolor='orange', facecolor='yellow', linewidth=6)
 ax.add_patch(polygon)
 ax.set_xlim(0, 69)
